f380622f31facdb8104a126f918e5e87048df769
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-08-22 12:31:30 +0300

    Add files via upload

---
+++ exercise_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+type config struct 
+{
+    char nameServers [][];
+    char blockList [][];
+    char blockAddress4 [];
+    char blockAddress6 [];
+    int configUpdate [1]= {0,1};
+    //time update
+}
+int main()
+{
+
+}
